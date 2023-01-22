6e6e353498a073474017c882d57e089f5e6cb0a6
benjamineke <benjiemondia198@gmail.com>
2023-01-23 00:13:27 +0800

    Update login.php

--- login.php
+++ login.php
@@ -35,6 +35,7 @@
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/style.css">
+        <link rel="shortcut icon" href="img/BenjaminekeLogo.png">
         <title>Login System</title>       
     </head>
     <body>
